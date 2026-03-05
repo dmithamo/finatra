6eabcdf1c7487910b7055cab3e47ad92163fed27
dmithamo <33679184+dmithamo@users.noreply.github.com>
2026-03-05 16:28:59 +0300

    [chore] rename app because y!

---
+++ src/lib/components/app-footer.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import AppLogo from './app-logo.svelte';
+</script>
+
+<footer class="flex h-24 w-full flex-col items-center justify-center gap-1 border-t px-4 text-xs">
+	<AppLogo />
+	<p class="opacity-75">
+		Designed and developed by {' '}<a
+			class="underline underline-offset-3"
+			href="https://github.com/dmithamo"
+			target="_blank"
+			rel="norefferer">D Mithamo</a
+		>
+	</p>
+</footer>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,19 +1,12 @@
 <script>
+	import AppFooter from '$lib/components/app-footer.svelte';
 	import AppLogo from '$lib/components/app-logo.svelte';
 </script>
 
-<div class="flex h-full flex-col items-center justify-center">
-	<h1 class="text-4xl font-bold">Welcome to{' '}<AppLogo isNameOnly={true} />!</h1>
-	<p>Your privacy-preserving financial health companion (with AI&trade; and stuff)</p>
+<div class="flex h-full flex-col items-center justify-center gap-1 p-6">
+	<h1 class="text-2xl font-bold">Welcome to{' '}<AppLogo isNameOnly={true} />!</h1>
+	<p class="text-center text-sm">
+		Your privacy-preserving financial health companion (with AI&trade; and stuff)
+	</p>
 </div>
-<footer class="flex h-24 w-full items-center justify-center gap-4 border-t px-4 text-xs">
-	<AppLogo />
-	<span class="opacity-75"
-		>Designed and developed by {' '}<a
-			class="underline underline-offset-3"
-			href="https://github.com/dmithamo"
-			target="_blank"
-			rel="norefferer">D Mithamo</a
-		></span
-	>
-</footer>
+<AppFooter />
